716ec91eea0a91fa2e836f5211b3b3532ed6d503
Mo'men_Ahmed <momenahmed3615@gmail.com>
2026-08-16 02:15:30 +0300

    update PlannedMeal.kt

--- app/src/main/java/com/example/testapp/PlannedMeal.kt
+++ app/src/main/java/com/example/testapp/PlannedMeal.kt
@@ -1,4 +1,19 @@
 package com.example.testapp
 
-class PlannedMeal {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "planned_meals")
+data class PlannedMeal(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val mealId: String,
+
+    val mealName: String,
+
+    val mealImage: String,
+
+    val day: String
+)
