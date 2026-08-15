0ce2cf3e8b4b81cf3462563971d315854ecf7d77
Mo'men_Ahmed <momenahmed3615@gmail.com>
2026-08-16 01:35:54 +0300

    add FavoriteMeal.kt

---
+++ app/src/main/java/com/example/testapp/FavoriteMeal.kt
@@ -0,0 +1,20 @@
+package com.example.testapp
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorites")
+data class FavoriteMeal(
+
+    @PrimaryKey
+    val id: String,
+
+    val name: String,
+
+    val image: String,
+
+    val country: String? = null,
+
+    val instructions: String? = null
+)
